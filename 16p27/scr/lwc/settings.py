fc13c5a9cebfac2122b8c0f3d494cf47cd4f8d79
leoChristofoli <leochp@gmail.com>
2015-04-25 14:13:07 -0300

    15 - 15.04.25

--- 16p27/scr/lwc/settings.py
+++ 16p27/scr/lwc/settings.py
@@ -91,3 +91,13 @@ TEMPLATE_DIRS = (
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'static_dirs'),
+    )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+MEDIA_URL = '/media/'
